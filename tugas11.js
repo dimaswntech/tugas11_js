dcb86c15fe8a27fe6eb2db971cc15d59c5079e22
dimaswntech <67653363+dimaswntech@users.noreply.github.com>
2021-12-21 16:07:14 +0700

    Add files via upload

---
+++ tugas11.js
@@ -0,0 +1,17 @@
+// 1. Pastikan Teman Teman Sudah Menginstall Node JS
+
+// 2. Teman - Teman Buatlah File di Folder javascript teman teman sebuah file dengan nama file tugas11.js
+
+// 3. Buatlah Program Dengan Spesifikasi dibawah ini :
+
+// Teman teman buatlah sebuah perhitungan dengan penjumlahan (+) , perkalian (*) dan pembagian (/)
+// Gunakan lah 4 angka ini 2, 4, 5 dan 10 sehingga menghasilkan output 25 seperti dibawah ini
+
+
+// Hint : Pahami urutan operator dalam javascript
+
+// 4. Happy Codding :)
+
+var angka =[2,4,5,10];
+var x= (angka[1]/angka[0])*angka[3]+angka[2];
+console.log(x);
